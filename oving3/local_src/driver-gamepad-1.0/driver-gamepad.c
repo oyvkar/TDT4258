469ffa87b01256e45b8fb5965a6987ecb9ad9db9
Øyvind Karlsen <oyvkar@OagadouguBarbar.samfundet.no>
2015-03-17 12:14:04 +0100

    Skrevet mesteparten av gamepaddriveren, mangler funksjoner for lesing fra userspace. OBS: endringer er gjort i efm32gg.h filen

--- oving3/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ oving3/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -1,10 +1,32 @@
 /*
- * This is a demo Linux kernel module.
+ * This is the Linux kernel module for a gamepad provided in the course TDT4258
  */
 
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/ioport.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/interrupt.h>
+
+
+#include <asm/io.h>
+#include <asm/uaccess.h>
+
+
+#include "efm32gg.h"
+
+
+dev_t devNumber;
+unsigned int devCount = 1;
+struct cdev *buttons_cdev;
+struct class *cl;
+void __iomem *gpio_mem;
+void __iomem *gpio_portc_mem;
 
 /*
  * template_init - function to insert this module into kernel space
@@ -15,9 +37,103 @@
  * Returns 0 if successfull, otherwise -1
  */
 
-static int __init template_init(void)
+static struct file_operations fops {
+	.owner= THIS_MODULE,
+//	.llseek = gamepad_llseek,
+	.read = gamepad_read,
+	.mmap = gamepad_map,
+	.open = gamepad_open,
+	.release = gamepad_release,
+};
+	
+static int __init gamepad_driver_init(void)
 {
 	printk("Hello World, here is your module speaking\n");
+
+	// Create chardevice and device node
+	int error = alloc_chardev_region(&devNumber, 0, devCount, "GPIO_buttons"
+	if(error < 0)
+	{
+		printk(KERN_ERR "Character device region allocation FAILED, returning.\n");
+		return -1;
+	}
+	cl = class_create(THIS_MODULE, "GPIO_buttons");
+	device_create(cl,NULL,devNumber, NULL, "GPIO_buttons");
+	
+
+	//Request memory region access for GPIO functions and port C, and check if the driver is in use by other processes
+	
+	
+	struct resource *GPIO_resource = request_mem_region(GPIO_PA_BASE + 0x100, 0x20,"GPIO_functions"); //Using PA-adress with offset 0x100 to access GPIO functions
+	if(GPIO_resource == 0)
+	{
+		printk(KERN_ERR "Port A(GPIO functions) memory request FAILED, returning\n");
+		return -1;
+	}
+	struct resource *portC_resource = request_mem_region(GPIO_PC_BASE, 0x24, "GPIO_port_c");
+	if(portC_resource == 0)
+	{
+		printk(KERN_ERR "Port C memory request FAILED, returning\n");
+		return -1;
+	}
+	
+
+	// Remap to virtual addresses
+	gpio_mem = ioremap_nocache(GPIO_PA_BASE+0x100, 0x20);  //Using the PA-address to access general GPIO functions with offset 0x100
+	printk(KERN_DEBUG "gpio_mem_addr: %p\n", gpio_mem);
+	if(gpio_mem == 0)
+	{
+		printk(KERN_ERR "Port A(GPIO) remap failed, returning\n");
+		return -1;
+	}
+	gpio_portc_mem = ioremap_nocache(GPIO_PC_BASE, 0x24);
+	printk(KERN_DEBUG "gpio_portc_mem_addr: %p\n", gpio_portc_mem);
+	if(gpio_portc_mem == 0)
+	{
+		printk(KERN_ERR "Port C remap failed, returning\n");
+		return -1;
+	}
+
+	//Configure interrupts and GPIO, same procedure as ex1 and ex2
+
+	iowrite32(0x33333333,   gpio_portc_mem + GPIO_PC_MODEL);
+	iowrite32(0xff, 	gpio_portc_mem + GPIO_PC_DOUT);
+	iowrite32(0x22222222,   gpio_mem + GPIO_EXTIPSELL);
+	iowrite32(0xff, 	gpio_mem + GPIO_EXTIRISE);
+	iowrite32(0xff, 	gpio_mem + GPIO_EXTIFALL);
+	iowrite32(0xff, 	gpio_mem + GPIO_IEN);
+
+	// Setup GPIO IRQ handler, 17 and 18 are odd and even interrupts
+	
+	error = request_irq(17, interrupt_handler, 0, "GPIO_buttons",NULL);
+	if(error < 0)
+	{
+		printk(KERN_ERR "IRQ 1 request FAILED, returning \n");
+		return -1;
+	}
+	error = request_irq(18, interrupt_handler, 0, "GPIO_buttons", NULL);
+	if(error < 0)
+	{
+		printk(KERN_ERR "IRQ 2 request FAILED, returning \n");
+		return -1;
+	}
+
+	//might be wise to clear interrupt flags here
+
+
+
+	//Activate driver and register allocations
+	buttons_cdev = cdev_alloc();
+	buttons_cdev->owner = THIS_MODULE;
+	buttons_cdev->ops = &fops;
+	error = cdev_add(buttons_cdev,devNumber, devCount);
+	if(error == 0)
+	{
+		printk(KERN_ERR "Char device activation failed, returning\n");
+		return -1;
+	}
+
+
 	return 0;
 }
 
@@ -28,14 +144,79 @@ static int __init template_init(void)
  * code from a running kernel
  */
 
-static void __exit template_cleanup(void)
+static void __exit gamepad_driver_cleanup(void)
 {
 	 printk("Short life for a small module...\n");
+
+	//Deactivate driver
+	printk(KERN_DEBUG "Deactivate driver\n");
+	cdev_del(buttons_cdev);
+	//Free even and odd interrupts
+	free_irq(17,NULL);
+	free_irq(18,NULL);
+
+	//Disable GPIO interrupts
+	printk(KERN_DEBUG "Disable GPIUO interrupts\n");
+	iowrite32(0x0, gpio_mem + GPIO_IEN);
+	iowrite32(0x0, gpio_mem + GPIO_EXTIRISE);
+	iowrite32(0x0, gpio_mem + GPIO_EXTIFALL);
+
+	printk(KERN_DEBUG "Unmap GPIO\n");
+	iounmap(gpio_mem);
+	iounmap(gpio_portc_mem);
+
+	//Release memory
+	printk(KERN_DEBUG "Release memory region\n";
+	release_mem_region(GPIO_PA_BASE + 0x100, 0x20);
+	release_mem_region(GPIO_PC_BASE, 0x24);
+
+	//Destroy class and device
+	printk(KERN_DEBUG "Destroy class and device\n");
+	device_destroy(cl,devNumber);
+	class_destroy(cl);
+
+	//Unregister char device region
+	printk(KERN_DEBUG "Unregister char device region\n");
+	unregister_chrdev_region(devNumber, devCount);
 }
 
-module_init(template_init);
-module_exit(template_cleanup);
+/* Functions for using the gamepad from userspace */
 
-MODULE_DESCRIPTION("Small module, demo only, not very useful.");
+int gamepad_open(struct inode *inode,struct file *file){
+	return 0; //Configuration handled by the module_init
+}
+
+int gamepad_release(struct inode *inode, struct file *file){
+	return 0; //Configuration handled by the module_exit
+}
+
+
+
+
+/*
+MISSING:
+
+
+gamepad_read
+- reads data from the driver
+
+gamepad_llseek(?)
+- ???????
+
+gamepad_mmap
+- maps memory to userspace program
+
+interrupt_handler
+- handles interrupts
+
+
+*/
+
+
+
+module_init(gamepad_driver_init);
+module_exit(gamepad_driver_cleanup);
+
+MODULE_DESCRIPTION("Module for controlling a gamepad provided in the course TDT4258 at NTNU");
 MODULE_LICENSE("GPL");
 
--- oving3/local_src/driver-gamepad-1.0/efm32gg.h
+++ oving3/local_src/driver-gamepad-1.0/efm32gg.h
@@ -26,22 +26,22 @@
 #define GPIO_PB_DIN      ((volatile uint32_t*)(GPIO_PB_BASE + 0x1c))
 #define GPIO_PB_PINLOCKN ((volatile uint32_t*)(GPIO_PB_BASE + 0x20))
 
-#define GPIO_PC_CTRL     ((volatile uint32_t*)(GPIO_PC_BASE + 0x00))
-#define GPIO_PC_MODEL    ((volatile uint32_t*)(GPIO_PC_BASE + 0x04))
+#define GPIO_PC_CTRL     ((volatile uint32_t*)(0x00))
+#define GPIO_PC_MODEL    ((volatile uint32_t*)(0x04))
 #define GPIO_PC_MODEH    ((volatile uint32_t*)(GPIO_PC_BASE + 0x08))
-#define GPIO_PC_DOUT     ((volatile uint32_t*)(GPIO_PC_BASE + 0x0c))
+#define GPIO_PC_DOUT     ((volatile uint32_t*)(0x0c))
 #define GPIO_PC_DOUTSET  ((volatile uint32_t*)(GPIO_PC_BASE + 0x10))
 #define GPIO_PC_DOUTCLR  ((volatile uint32_t*)(GPIO_PC_BASE + 0x14))
 #define GPIO_PC_DOUTTGL  ((volatile uint32_t*)(GPIO_PC_BASE + 0x18))
-#define GPIO_PC_DIN      ((volatile uint32_t*)(GPIO_PC_BASE + 0x1c))
+#define GPIO_PC_DIN      ((volatile uint32_t*)(0x1c))
 #define GPIO_PC_PINLOCKN ((volatile uint32_t*)(GPIO_PC_BASE + 0x20))
 
-#define GPIO_EXTIPSELL ((volatile uint32_t*)(GPIO_PA_BASE + 0x100))
-#define GPIO_EXTIPSELH ((volatile uint32_t*)(GPIO_PA_BASE + 0x104))
-#define GPIO_EXTIRISE  ((volatile uint32_t*)(GPIO_PA_BASE + 0x108))
-#define GPIO_EXTIFALL  ((volatile uint32_t*)(GPIO_PA_BASE + 0x10c))
-#define GPIO_IEN       ((volatile uint32_t*)(GPIO_PA_BASE + 0x110))
-#define GPIO_IFC       ((volatile uint32_t*)(GPIO_PA_BASE + 0x11c))
+#define GPIO_EXTIPSELL ((volatile uint32_t*)(0x100))
+#define GPIO_EXTIPSELH ((volatile uint32_t*)(0x104))
+#define GPIO_EXTIRISE  ((volatile uint32_t*)(0x108))
+#define GPIO_EXTIFALL  ((volatile uint32_t*)(0x10c))
+#define GPIO_IEN       ((volatile uint32_t*)(0x110))
+#define GPIO_IFC       ((volatile uint32_t*)(0x11c))
 
 // CMU
 
