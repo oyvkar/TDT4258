f7a9d36e64740b4c715c810010b8cc6ab2a6da72
Øyvind Karlsen <oyvkar@samfundet.no>
2015-04-14 13:59:28 +0200

    gogogogogo

--- oving3/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ oving3/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -202,7 +202,7 @@ static ssize_t my_write (struct file *filp, const char __user *buff, size_t coun
     return 0; //Not used as we do not want to write to the LEDs on the gamepad
 }
 
-static irqreturn_t interrupt_handler(int irq, void *dev_id, struct pt_regs *regs){
+static irq_handler_t interrupt_handler(int irq, void *dev_id, struct pt_regs *regs){
     //TODO: handle interrupts
     memwrite(gpio, GPIO_IFC, 0xffff);//Clear interrupt flags
     
