debfe3d804fd26606ca95321d8f793af2b2729a0
Ivar Eriksen <ivar@flyndre.com>
2015-04-20 14:48:33 +0200

    Improved button map

--- oving3/local_src/driver-gamepad-1.0/driver-gamepad.h
+++ oving3/local_src/driver-gamepad-1.0/driver-gamepad.h
@@ -8,7 +8,7 @@ static ssize_t gp_read(struct file *filp, char __user *buff, size_t count, loff_
 static ssize_t gp_write(struct file *filp, const char __user *buff, size_t count, loff_t *offp);
 static irq_handler_t interrupt_handler(int irq, void *dev_id, struct pt_regs *regs);
 static void button_map(void);
-static void gp_fasync(void);
+static int gp_fasync(int fd, struct file *filp, int mode);
 
 
 
