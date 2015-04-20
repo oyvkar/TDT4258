211e5dbb29125711b7f0f6fc608798bfb3456e74
Øyvind Karlsen <oyvkar@samfundet.no>
2015-04-20 16:50:50 +0200

    fuckityfuck

--- oving3/local_src/game-1.0/game.c
+++ oving3/local_src/game-1.0/game.c
@@ -153,7 +153,7 @@ void moveball(){
 
 void handlePhysics(){
     if (ball_a.Xpos - ball_a.radius <= 30) {   //Checks if the ball hits the bat
-        if ((ball_a.Ypos > playerbat_a.Ypos) && (ball_a.Ypos > (playerbat_a.Ypos + playerbat_a.length))) {
+        if ((ball_a.Ypos < playerbat_a.Ypos) && (ball_a.Ypos < (playerbat_a.Ypos + playerbat_a.length))) {
             ball_a.Xspeed = -ball_a.Xspeed; //Ball was hit, reverse the speed
             ball_a.Yspeed = rand()%4;//Random Y speed
             if(rand()%2 == 0)ball_a.Yspeed = -ball_a.Yspeed;//Random Y direction
