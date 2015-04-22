bc4dc546eb88e5e6633055471649d35d8417d5e2
Ivar Eriksen <ivar@flyndre.com>
2015-04-22 23:28:32 +0200

    Attempted coordinate fix #10

--- oving3/local_src/game-1.0/game.c
+++ oving3/local_src/game-1.0/game.c
@@ -110,9 +110,9 @@ void moveBat(Playerbat_t *bat, bool down) {
         printf("down: %i\n",down);
         printf("Bat: ypos: %i\tLength: %i\tSpeed: %i\n", bat->Ypos, bat->width, bat->speed);
         printf("ypos + len: %i\typos+len+speed %i\typos-len %i\n", bat->Ypos + bat->length, bat->Ypos + bat->length + bat->speed, bat->Ypos - bat->length);
-        draw_rectangle(bat->Xpos, bat->Ypos, bat->width, bat->length, black);
+        draw_rectangle(bat->Xpos, bat->Ypos + bat->length + 1, bat->width, bat->speed, black);
+        draw_rectangle(bat->Xpos, bat->Ypos -1 , bat->width, bat->speed, white);
         bat->Ypos -= bat->speed;
-        draw_rectangle(bat->Xpos, bat->Ypos, bat->width, bat->length, white);
     }
 }
 
